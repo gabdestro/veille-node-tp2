67b4eb6995819095081ce00da8c7d87ccbcce4a7
Gabriel Armstrong <gabriel.armstrong@lepisc.com>
2018-03-20 17:00:50 -0400

    tp2-UP

---
+++ .gitignore.js
@@ -0,0 +1,6 @@
+node_modules/
+public/css/*.css
+!public/css/normalize.css
+data_mongo/
+package-lock
+tp1-Node-Express-Mongodb.pdf
---
+++ mes_modules/chat_socket/index.js
@@ -0,0 +1,14 @@
+let util = require('util')
+let socketio = require('socket.io')
+
+module.exports.listen = function(server){
+    let io = socketio.listen(server)
+
+    // ------------------------------ Traitement du socket
+    let objUtilisateur = {}
+    io.on('connection', function(socket){
+    console.log(socket.id)
+    // .......
+   })
+ return io
+}
